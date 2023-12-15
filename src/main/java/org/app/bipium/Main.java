57368f5b52435bb2a0299982411be1275e3f2217
av.silichev <dev.silichev1994@gmail.com>
2023-12-15 17:45:27 +0300

    project refactoring 3.1

--- src/main/java/org/app/bipium/Main.java
+++ src/main/java/org/app/bipium/Main.java
@@ -1,15 +1,7 @@
 package org.app.bipium;
 
-import org.app.bipium.models.catalogs.*;
 import org.app.bipium.models.devices.AbstractDevice;
-import org.app.bipium.services.BipiumApiServices;
 import org.app.bipium.services.BipiumApiServicesBuilder;
-import org.app.bipium.services.BipiumApiServicesImpl;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
